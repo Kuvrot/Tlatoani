4c28ba05499b6ebb146939f0dcd59073ba588629
Kuvrot <jamessanz@outlook.com>
2024-02-23 18:25:29 -0600

    added progress bar to farms

--- Assets/Prototype/Scripts/Farm.cs
+++ Assets/Prototype/Scripts/Farm.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Farm : MonoBehaviour
 {
 
     public float time = 60;
+    Image progressImage;
+    float clock = 0;
 
     // Update is called once per frame
     private void Start()
     {
+        progressImage = GetComponentInChildren<Image>();
         StartCoroutine(Timer());
+    }
+
+    private void Update()
+    {
+        progressImage.fillAmount = clock/ time;
+        clock += 1 * Time.deltaTime;
+
+        if (clock >= time)
+        {
+            clock = 0;
+        }
+
+
     }
 
     IEnumerator Timer()
@@ -20,7 +37,7 @@ public class Farm : MonoBehaviour
 
         if (BuildingManager.instance.currentResources[0] >= 30)
         {
-            BuildingManager.instance.AddResource(ResourceType.Food, 10);
+            BuildingManager.instance.AddResource(ResourceType.Food, 20);
             BuildingManager.instance.AddResource(ResourceType.Wood, -30);
             BuildingManager.instance.RefreshResources();
         }
